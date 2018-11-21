827821268290f59ab77fb3697c2cbcdd0c38c35a
adamjorycromer <adamjorycromer@gmail.com>
2018-11-20 19:48:46 -0700

    added answers

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,4 +1,64 @@
 $(document).ready(function () {
 
-
+    var question1 = {        
+        question="",
+        answers=[],
+        correctAnswer=0,
+        images=[]
+    }
+    var question2 = {        
+        question="",
+        answers=[],
+        correctAnswer=0,
+        images=[]
+    }
+    var question3 = {        
+        question="",
+        answers=[],
+        correctAnswer=0,
+        images=[]
+    }
+    var question4 = {        
+        question="",
+        answers=[],
+        correctAnswer=0,
+        images=[]
+    }
+    var question5 = {        
+        question="",
+        answers=[],
+        correctAnswer=0,
+        images=[]
+    }
+    var question6 = {        
+        question="What is a nihilist?",
+        answers=["A person who believes that life is meaningless.", "A person who disbelieves the existence of God", "A person who believes that nothing is known or can be known of the existence or nature of God.", ],
+        correctAnswer=0,
+        images=[]
+    }
+    var question7 = {        
+        question="",
+        answers=[],
+        correctAnswer=0,
+        images=[]
+    }
+    var question8 = {        
+        question="",
+        answers=[],
+        correctAnswer=0,
+        images=[]
+    }
+    var question9 = {        
+        question="",
+        answers=[],
+        correctAnswer=0,
+        images=[]
+    }
+    var question10 = {        
+        question="",
+        answers=[],
+        correctAnswer=0,
+        images=[]
+    }
+    
 });
