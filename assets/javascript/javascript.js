49dc7d7b9a447d689a0c7e38be41be855e859652
adamjorycromer <adamjorycromer@gmail.com>
2018-11-21 20:56:10 -0700

    questions finished

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,64 +1,67 @@
 $(document).ready(function () {
 
+    //Create objects which includes the questions, answers in an array, and the correct index number of the right answer.
     var question1 = {        
-        question="",
-        answers=[],
-        correctAnswer=0,
-        images=[]
+        question = "What really tied the room together?",
+        answers = ["A Sofa", "A Rug", "A Painting", "An Antique Rocking Chair"],
+        correctAnswer = 1,
+        images = []
     }
     var question2 = {        
-        question="",
-        answers=[],
-        correctAnswer=0,
-        images=[]
+        question = "What body part can Walter get you by 3PM?",
+        answers = ["A Toe", "A Finger", "A Head", "An Eye"],
+        correctAnswer = 0,
+        images = []
     }
     var question3 = {        
-        question="",
-        answers=[],
-        correctAnswer=0,
-        images=[]
+        question = "What is The Dude buying at the start of the movie with a check for $0.69?",
+        answers = ["Coffee", "Milk", "Half & Half", "A Coke"],
+        correctAnswer = 2,
+        images = []
     }
     var question4 = {        
-        question="",
-        answers=[],
-        correctAnswer=0,
+        question="What word is said the most throughout the film?",
+        answers=["Dude", "Man", "The F Word", "Bowling"],
+        correctAnswer = 2,
         images=[]
     }
-    var question5 = {        
-        question="",
-        answers=[],
-        correctAnswer=0,
-        images=[]
+    var question5 = { 
+        question = "What band does The Dude hate?",
+        answers = ["The Beatles", "Fleetwood Mac", "The Eagles", "The Bangles"],
+        correctAnswer = 2,
+        images = [""]
     }
     var question6 = {        
         question="What is a nihilist?",
-        answers=["A person who believes that life is meaningless.", "A person who disbelieves the existence of God", "A person who believes that nothing is known or can be known of the existence or nature of God.", ],
+        answers=["A person who believes that life is meaningless.", "A person who disbelieves the existence of God", "A person who believes that nothing is known or can be known of the existence of God.", "A person who believes in multiple Gods."],
         correctAnswer=0,
         images=[]
     }
     var question7 = {        
-        question="",
-        answers=[],
-        correctAnswer=0,
+        question = "Who is present in every scene?",
+        answers = ["Walter", "Bunny", "Donnie", "The Dude"],
+        correctAnswer = 3,
         images=[]
     }
     var question8 = {        
-        question="",
-        answers=[],
+        question="Why can't Walter answer his phone?",
+        answers=["He's observing Shabbos.", "He's in PTSD therapy.", "He forgot to pay his bill.", "He doesn't use a phone because he believes he's being tracked by the CIA."],
         correctAnswer=0,
         images=[]
     }
     var question9 = {        
-        question="",
-        answers=[],
-        correctAnswer=0,
-        images=[]
+        question = "Jackie Treehorn treats objects like what according to The Dude?",
+        answers = ["Books", "Dogs", "Food", "Women"],
+        correctAnswer = 0,
+        images = []
     }
     var question10 = {        
-        question="",
-        answers=[],
-        correctAnswer=0,
+        question = "Who is The Big Lebowski?",
+        answers = ['Jeffrey "The Dude" Lebowski', "Bunny Lebowski", "Maude Lebowski", 'Jeffrey "The Millionaire" Lebowski'],
+        correctAnswer = 3,
         images=[]
     }
+
+    
     
 });
